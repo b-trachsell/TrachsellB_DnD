fd1a4a22bfb4d66fafd9b6e692248ff9c6da9b1d
Trachsell <b_trachsell@ats-hr420-dm35.fc.ca>
2020-01-31 13:31:50 -0500

    Working w/ js Background and images on click

--- script/app.js
+++ script/app.js
@@ -1,11 +1,28 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+				puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+				gameBoard = document.querySelector('.puzzle-board');
+
+	const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
+
 
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
-		// and set the drop zone background
+		// and set the drop zone background image based on what the user selects
 
+		//change elements on the left to match bg
+
+
+
+
+
+		// Button below the esc key for those weird quotes
+		//.style lets you add css whithin JS
+		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`);
+
+		gameBoard.style.backgroundImage = `url(images/background${this.dataset.puzzleref}.jpg)`;
 		debugger;
 	}
 
@@ -14,4 +31,7 @@
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	//RESEARCH CALL APPLY AND BIND
+	changeImageSet.call(puzzleButtons[0]);
 })();
